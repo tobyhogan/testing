7317070f7068e1e427fdf356307bc57016b7c631
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2023-11-24 12:56:10 +0000

    .

--- {{ACTUALlTESTING}}/z {{ACTIVE}}/react-chartjs/src/App.js
+++ {{ACTUALlTESTING}}/z {{ACTIVE}}/react-chartjs/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from "chart.js"
+import { Bar } from "react-chartjs-2"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { faker } from '@faker-js/faker';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top"
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart"
+    }
+  }
 }
 
-export default App;
+const labels = ["January", "February", "March", "April", "May", "June", "July"]
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "rgba(255, 99, 132, 0.5)"
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: "rgba(53, 162, 235, 0.5)"
+    }
+  ]
+}
+
+export function App() {
+  return <Bar options={options} data={data} />
+}
