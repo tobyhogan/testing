e19d9144cbfe4584909ce3ae299752127df89fdc
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-04-06 06:38:05 +0100

    .

--- {{MAIN}}/z {{ACTIVE}}/{{REACT}}/src/{{TESTING}}/react-get-input/1.js
+++ {{MAIN}}/z {{ACTIVE}}/{{REACT}}/src/{{TESTING}}/react-get-input/1.js
@@ -1,53 +1,30 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.reset();
+    }
 
+    reset() {
+        // Always set the initial state in its own function, so that
+        // you can trivially reset your components at any point.
+        this.state = {
+            inputValue: ''
+        };
+    }
 
-import './App.css';
+    render() {
+        return (
+            // ...
+            <input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
+            // ...
+        );
+    },
 
-function App() {
-
-    
-    class MyComponent extends React.Component {
-        constructor(props) {
-            super(props);
-            this.reset();
-        }
-    
-        reset() {
-            // Always set the initial state in its own function, so that
-            // you can trivially reset your components at any point.
-            this.state = {
-                inputValue: ''
-            };
-        }
-    
-        render() {
-            return (
-                // ...
-                <input value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)}/>
-                // ...
-            );
-        },
-    
-        updateInputValue(evt) {
-            const val = evt.target.value;
-            // ...       
-            this.setState({
-                inputValue: val
-            });
-        }
-    });
-
-
-
-
-  return (
-    <div className="App">
-
-      <h1>hi</h1>
-
-    </div>
-  );
-}
-
-export default App;
-
-
+    updateInputValue(evt) {
+        const val = evt.target.value;
+        // ...       
+        this.setState({
+            inputValue: val
+        });
+    }
+});
