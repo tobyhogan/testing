17afb06d5e2f72ba8ba451346a3df4d264f36afe
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-09 21:22:25 +0100

    .

--- {{MAIN}}/{{REACT}}/{{ TSX-REACT-VITE-GENERAL }}/vite.config.ts
+++ {{MAIN}}/{{REACT}}/{{ TSX-REACT-VITE-GENERAL }}/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import rollupReplace from '@rollup/plugin-replace'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    rollupReplace({
+      preventAssignment: true,
+      values: {
+        __DEV__: JSON.stringify(true),
+        'process.env.NODE_ENV': JSON.stringify('development'),
+      },
+    }),
+    react(),
+  ],
 })
