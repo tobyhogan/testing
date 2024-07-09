44cc33990b6bf9e22d4d0690bf902d872ad7b5fd
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-09 21:10:59 +0100

    .

---
+++ {{MAIN}}/{{REACT}}/{{ TSX-REACT-VITE-GENERAL }}/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-blue-500 text-5xl mx-auto mt-5 px-10 py-4 w-fit'>React. Vite. Tailwind.</h1>
+    </>
+  )
+}
+
+export default App
