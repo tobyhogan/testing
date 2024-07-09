4095d20ce74d23aeb1e1f461641fb9186507a760
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-09 21:44:11 +0100

    .

--- {{MAIN}}/{{REACT}}/{{ TSX-REACT-VITE-GENERAL }}/src/tanstack-row-selection/file1.tsx
+++ {{MAIN}}/{{REACT}}/{{ TSX-REACT-VITE-GENERAL }}/src/tanstack-row-selection/file1.tsx
@@ -34,10 +34,12 @@ function App() {
   // useReducer is similar to useState, but for states with more copmlex logic
 
   const rerender = React.useReducer(() => ({}), {})[1]
-  
+
 
   const [rowSelection, setRowSelection] = React.useState({})
   const [globalFilter, setGlobalFilter] = React.useState('')
+
+  // useMemo allows for caching of frequently re-used values
 
   const columns = React.useMemo<ColumnDef<Person>[]>(
     () => [
@@ -120,6 +122,8 @@ function App() {
 
   const [data, setData] = React.useState(() => makeData(100000))
   const refreshData = () => setData(() => makeData(100000))
+
+  // tanstack table instantiation
 
   const table = useReactTable({
     data,
