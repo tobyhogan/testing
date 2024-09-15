6976023204914fab868be5801e2d435861cd3141
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-09-15 09:14:49 +0100

    .

--- {{MAIN}}/{{REACT}}/{{ TSX-REACT-VITE-GENERAL }}/src/App.tsx
+++ {{MAIN}}/{{REACT}}/{{ TSX-REACT-VITE-GENERAL }}/src/App.tsx
@@ -1,22 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import Main from './contextmenugrid/Main'
+import Main from './contextmenugrid/Main.tsx'
 
 function App() {
 
   return (
+    <>
 
-    
-
-    <div className='bg-blue-700'>hi</div>
-
+      <Main />
+    </>
   )
 }
 
-export default App
+//       <h1 className='bg-blue-500 text-5xl mx-auto mt-5 px-10 py-4 w-fit'>React. Vite. Tailwind.</h1>
 
-/*
-<>
-  <h1 className='bg-blue-500 text-5xl mx-auto mt-5 px-10 py-4 w-fit'>React. Vite. Tailwind.</h1>
-</>
-*/
+export default App
